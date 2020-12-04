0b64fa7446c96e672898b38be92a9251d855c216
AxelGard <axel.gard@protonmail.com>
2020-12-04 21:45:34 +0100

    test input

--- typist/src/App.js
+++ typist/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const InputPage = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-group">
+      <label htmlFor="formGroupExampleInput">Default input</label>
+      <input
+        type="text"
+        className="form-control"
+        id="formGroupExampleInput"
+      />
     </div>
   );
 }
 
-export default App;
+export default InputPage;
